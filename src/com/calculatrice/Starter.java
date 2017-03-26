afa0386780de8ebde10c47aed3125a9adc217118
StraetenMax <maxstraeten@yahoo.com>
2017-03-26 18:58:49 +0200

    mise en place du programme

--- src/com/calculatrice/Starter.java
+++ src/com/calculatrice/Starter.java
@@ -5,12 +5,21 @@ import java.util.Scanner;
 public class Starter {
 
 	public static void main(String[] args) {
-		float a,b, result;
+		float a, b, result;
 		char operator = '#', recommencer ='N';
 		
 		System.out.println("Bonjour et bienvenue dans la calculatrice");
 		//coeur du programme
-		do
+		do{
+			a = askFloat("Entrez le premier nombre");
+			while(operator != '+' && operator != '-' && operator != '/' && operator != '*'){
+				operator = askChar("Rentrez l'opérateur !");
+			}
+			b = askFloat("Entrez le deuxième nombre");
+			result = calculer(a, operator, b);
+			System.out.println("Le résultat de "+a+" "+operator+" "+b+" = "+result);
+			recommencer = askChar("Voulez-vous recommencer?(O/o pour recommence)");
+		}while(recommencer == 'O' || recommencer == 'o');
 	}
 	public static float askFloat(String message){
 		//déclarer variables
@@ -34,7 +43,7 @@ public class Starter {
 	return result;
 	}
 	
-	public static float askChar(String message){
+	public static char askChar(String message){
 		//déclarer les variables
 		Scanner clavier = new Scanner(System.in);
 		char result = 0;
