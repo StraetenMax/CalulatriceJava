505ab07a8a5166e00a5c96d5a4f0c97d9fe9a8dd
StraetenMax <maxstraeten@yahoo.com>
2017-03-26 16:54:18 +0200

    construction de la première méthode

---
+++ src/com/calculatrice/Starter.java
@@ -0,0 +1,21 @@
+package com.calculatrice;
+
+import java.util.Scanner;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static float askFloat(String message){
+		Scanner clavier = new Scanner(System.in);
+		float result = 0;
+		boolean isOk = false;
+		
+		while(!isOk){
+			System.out.println();
+		}
+	}
+	
+}
