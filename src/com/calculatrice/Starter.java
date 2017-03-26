6e26cdb6bddaed820b55e48d000ee2e158940627
StraetenMax <maxstraeten@yahoo.com>
2017-03-26 18:23:47 +0200

    mise enplace du programme les variable

--- src/com/calculatrice/Starter.java
+++ src/com/calculatrice/Starter.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class Starter {
 
 	public static void main(String[] args) {
-
+		float a,b, result;
+		char operator = '#', recommencer ='N';
+		
+		System.out.println("Bonjour et bienvenue dans la calculatrice");
+		//coeur du programme
+		do
 	}
 	public static float askFloat(String message){
 		//déclarer variables
@@ -29,5 +34,54 @@ public class Starter {
 	return result;
 	}
 	
-	
+	public static float askChar(String message){
+		//déclarer les variables
+		Scanner clavier = new Scanner(System.in);
+		char result = 0;
+		boolean isOk = false;
+		String line;
+		
+		//récupération du char
+		while(!isOk){
+			try{
+				//Instruction à faire
+				line = clavier.nextLine();
+				result = line.charAt(0);
+				isOk = true;
+			}catch(Exception e){
+				//Instruction à faire si problème
+				clavier.nextLine();
+				isOk = false;
+				
+			}
+		}
+	 return result;
+	}
+
+	public static float calculer(float a, char operator, float b){
+		// déclarer les variables
+		float result = 0;
+		switch(operator){
+		case '/':
+			if(b == 0){
+				System.out.println("Erreur: Division par zero !");
+				System.exit(0);
+			}
+			result = a / b;
+			break;
+		case '*':
+			result = a * b;
+			break;
+		case '+':
+			result = a + b;
+			break;
+		case '-':
+			result = a - b;
+			break;
+		default:
+			System.out.println("Erreur: opérateur inconnu");
+			System.exit(0);
+	}
+		return result;
+	}
 }
