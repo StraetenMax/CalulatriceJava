06ed56256ee7ea2076df854d09c2cf7bcf7e76fb
StraetenMax <maxstraeten@yahoo.com>
2017-03-26 17:02:09 +0200

    récupération du float

--- src/com/calculatrice/Starter.java
+++ src/com/calculatrice/Starter.java
@@ -13,8 +13,18 @@ public class Starter {
 		float result = 0;
 		boolean isOk = false;
 		
+		// récupération du float
 		while(!isOk){
 			System.out.println();
+		try{
+			//instruction à faire
+			result = clavier.nextFloat();
+			isOk = true;
+		}catch(Exception e){
+			//instruction à faire si probleme
+			clavier.nextLine();
+			isOk = false;
+		}
 		}
 	}
 	
